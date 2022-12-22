bd83587cc3c58dc28e98b4238079480523668b15
lafernandes22 <fernandesalucas2018@gmail.com>
2022-12-22 15:42:34 -0500

    more progress

--- 2022/day9.py
+++ 2022/day9.py
@@ -5,8 +5,8 @@ for line in f:
     line = line.split(' ')
     lines.append(line)
 
-length, width = 100, 100
-start_pos = (length//2, width//2)
+length, width = 21, 26
+start_pos = (15, 13)
 head_pos = start_pos
 tail_pos = start_pos
 visited = []
@@ -93,67 +93,78 @@ rope = {}
 for x in range(1, 10):
     rope[x] = head_pos
 
+def print_board_2(head):
+    pos = [head] + list(rope.values())
+    print(pos)
+    for x in range(length-1, -1, -1):
+        line = ''
+        for y in range(width):
+            if (x,y) in pos:
+                line += str(pos.index((x,y)))
+            elif (x,y) == head_pos:
+                 line += 'H'
+            else:
+                line += '.'
+        print(line)
 
 def move (head, tail, dir): # checking if tail needs to move relative to head
-    print(head, tail)
     match(dir):
         case 'R':
             if not check_distance(head, tail):
                 tail = (tail[0], tail[1] + 1)
                 if not check_ordinal(head, tail):
                     tail = (head[0], tail[1])
-                return True
+                return tail
         case 'L':
             if not check_distance(head, tail):
                 tail = (tail[0], tail[1] - 1)
                 if not check_ordinal(head, tail):
                     tail = (head[0], tail[1])
-                return True
+                return tail
         case 'U':
             if not check_distance(head, tail):
                 tail = (tail[0] + 1, tail[1])
                 if not check_ordinal(head, tail):
                     tail = (tail[0], head[1])
-                return True
+                return tail
         case 'D':
             if not check_distance(head, tail):
                 tail = (tail[0] - 1, tail[1])
                 if not check_ordinal(head, tail):
                     tail = (tail[0], head[1])
-                return True
-    return False
+                return tail
+    return -1
 
-def print_board_pt2():
-    for x in range(length-1, -1, -1):
-        line = ''
-        line_pos = ''
-        for y in range(width):
-            if (x,y) in visited:
-                line += '#'
-                if (x,y) == tail_pos:
-                    line_pos += 'T'
-                else:
-                    line_pos += '.'
-            elif (x,y) == head_pos:
-                 line_pos += 'H'
-                 line += '.'
-            else:
-                line += '.'
-                line_pos += '.'
-        print(line, line_pos)
-
+lines = lines[0:2]
 for line in lines:
     print('  == '+''.join(line)+' ==')
     dir = line[0]
-    tail_key = 1
     visited.append(rope[9])
     match(dir):
         case 'R':
             for _ in range(int(line[1])):
                 head_pos = (head_pos[0], head_pos[1] + 1)
+                tail_key = 1
                 curr_tail = rope[tail_key]
-                print(tail_key)
-                while move(head_pos, curr_tail, dir): # TODO: return new tail pos if true to modify
+                curr_head = head_pos
+                while move(curr_head, curr_tail, dir) != -1:
+                    pos = move(head_pos, curr_tail, dir)
+                    rope[tail_key] = pos
+                    curr_head = pos
+                    if tail_key == 9 and curr_tail not in visited:
+                        visited.append(tail_key)
+                    tail_key += 1
+                    curr_tail = rope[tail_key]
+        case 'U':
+            for _ in range(int(line[1])):
+                head_pos = (head_pos[0] + 1, head_pos[1])
+                tail_key = 1
+                curr_tail = rope[tail_key]
+                curr_head = head_pos
+                while move(curr_head, curr_tail, dir) != -1:
+                    pos = move(head_pos, curr_tail, dir)
+                    rope[tail_key] = pos
+                    curr_head = pos
                     if tail_key == 9 and curr_tail not in visited:
                         visited.append(tail_key)
                     tail_key += 1
@@ -161,8 +172,8 @@ for line in lines:
                         break
                     else:
                         curr_tail = rope[tail_key]
-                tail_key += 1
-                print(tail_key)
-    break
+    print(head_pos)
+    print(rope)
+    print_board_2(head_pos)
 
 
