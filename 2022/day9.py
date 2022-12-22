fc5ca9188ba8a90e572869bef914abe96e62d80d
lafernandes22 <fernandesalucas2018@gmail.com>
2022-12-22 14:27:56 -0500

    day9 pt2 in progress

--- 2022/day9.py
+++ 2022/day9.py
@@ -1,16 +1,15 @@
-f = open('2022/day9input.txt', 'r').readlines()
+f = open('2022/day9test.txt', 'r').readlines()
 lines = []
 for line in f:
     line = line.replace('\n', '')
     line = line.split(' ')
     lines.append(line)
 
-length, width = 5, 6
-# start_pos = (length//2, width//2)
-start_pos = (0,0)
+length, width = 100, 100
+start_pos = (length//2, width//2)
 head_pos = start_pos
 tail_pos = start_pos
-visited = [tail_pos]
+visited = []
 
 def check_distance(head, tail):
     if not -1 <= head[0] - tail[0] <= 1 or not -1 <= head[1] - tail[1] <= 1:
@@ -40,55 +39,130 @@ def print_board():
             else:
                 line += '.'
                 line_pos += '.'
-        # print(line, line_pos)
+        print(line, line_pos)
+
+def pt1():
+    visited.append(tail_pos)
+    for line in lines:
+        print('  == '+''.join(line)+' ==')
+        match(line[0]): 
+            case 'R':
+                for _ in range(int(line[1])):
+                    head_pos = (head_pos[0], head_pos[1] + 1)
+                    if not check_distance(head_pos, tail_pos):
+                        tail_pos = (tail_pos[0], tail_pos[1] + 1)
+                        if not check_ordinal(head_pos, tail_pos):
+                            tail_pos = (head_pos[0], tail_pos[1])
+                        if tail_pos not in visited:
+                            visited.append(tail_pos)
+                            print_board()
+            case 'L':
+                for _ in range(int(line[1])):
+                        head_pos = (head_pos[0], head_pos[1] - 1)
+                        if not check_distance(head_pos, tail_pos):
+                            tail_pos = (tail_pos[0], tail_pos[1] - 1)
+                            if not check_ordinal(head_pos, tail_pos):
+                                tail_pos = (head_pos[0], tail_pos[1])
+                            if tail_pos not in visited:
+                                visited.append(tail_pos)
+                                print_board()
+            case 'U':
+                for _ in range(int(line[1])):
+                    head_pos = (head_pos[0] + 1, head_pos[1])
+                    if not check_distance(head_pos, tail_pos):
+                        tail_pos = (tail_pos[0] + 1, tail_pos[1])
+                        if not check_ordinal(head_pos, tail_pos):
+                            tail_pos = (tail_pos[0], head_pos[1])
+                        if tail_pos not in visited:
+                            visited.append(tail_pos)
+                            print_board()
+            case 'D':
+                for _ in range(int(line[1])):
+                    head_pos = (head_pos[0] - 1, head_pos[1])
+                    if not check_distance(head_pos, tail_pos):
+                        tail_pos = (tail_pos[0] - 1, tail_pos[1])
+                        if not check_ordinal(head_pos, tail_pos):
+                            tail_pos = (tail_pos[0], head_pos[1])
+                        if tail_pos not in visited:
+                            visited.append(tail_pos)
+                            print_board()
+    print_board()
+    print(len(visited))
+
+rope = {}
+for x in range(1, 10):
+    rope[x] = head_pos
+
+
+def move (head, tail, dir): # checking if tail needs to move relative to head
+    print(head, tail)
+    match(dir):
+        case 'R':
+            if not check_distance(head, tail):
+                tail = (tail[0], tail[1] + 1)
+                if not check_ordinal(head, tail):
+                    tail = (head[0], tail[1])
+                return True
+        case 'L':
+            if not check_distance(head, tail):
+                tail = (tail[0], tail[1] - 1)
+                if not check_ordinal(head, tail):
+                    tail = (head[0], tail[1])
+                return True
+        case 'U':
+            if not check_distance(head, tail):
+                tail = (tail[0] + 1, tail[1])
+                if not check_ordinal(head, tail):
+                    tail = (tail[0], head[1])
+                return True
+        case 'D':
+            if not check_distance(head, tail):
+                tail = (tail[0] - 1, tail[1])
+                if not check_ordinal(head, tail):
+                    tail = (tail[0], head[1])
+                return True
+    return False
+
+def print_board_pt2():
+    for x in range(length-1, -1, -1):
+        line = ''
+        line_pos = ''
+        for y in range(width):
+            if (x,y) in visited:
+                line += '#'
+                if (x,y) == tail_pos:
+                    line_pos += 'T'
+                else:
+                    line_pos += '.'
+            elif (x,y) == head_pos:
+                 line_pos += 'H'
+                 line += '.'
+            else:
+                line += '.'
+                line_pos += '.'
+        print(line, line_pos)
 
 for line in lines:
-    # TODO loop each motion and every step move tail as needed, track seen positions at each step
     print('  == '+''.join(line)+' ==')
-    # TODO ordinal check to move tail diagonally to reset to ordinal movement
-    match(line[0]): 
+    dir = line[0]
+    tail_key = 1
+    visited.append(rope[9])
+    match(dir):
         case 'R':
-            for x in range(int(line[1])):
+            for _ in range(int(line[1])):
                 head_pos = (head_pos[0], head_pos[1] + 1)
-                if not check_distance(head_pos, tail_pos):
-                    tail_pos = (tail_pos[0], tail_pos[1] + 1)
-                    if not check_ordinal(head_pos, tail_pos):
-                        tail_pos = (head_pos[0], tail_pos[1])
-                    if tail_pos not in visited:
-                        visited.append(tail_pos)
-                        print_board()
-        case 'L':
-           for x in range(int(line[1])):
-                head_pos = (head_pos[0], head_pos[1] - 1)
-                if not check_distance(head_pos, tail_pos):
-                    tail_pos = (tail_pos[0], tail_pos[1] - 1)
-                    if not check_ordinal(head_pos, tail_pos):
-                        tail_pos = (head_pos[0], tail_pos[1])
-                    if tail_pos not in visited:
-                        visited.append(tail_pos)
-                        print_board()
-        case 'U':
-            for x in range(int(line[1])):
-                head_pos = (head_pos[0] + 1, head_pos[1])
-                if not check_distance(head_pos, tail_pos):
-                    tail_pos = (tail_pos[0] + 1, tail_pos[1])
-                    if not check_ordinal(head_pos, tail_pos):
-                        tail_pos = (tail_pos[0], head_pos[1])
-                    if tail_pos not in visited:
-                        visited.append(tail_pos)
-                        print_board()
-        case 'D':
-            for x in range(int(line[1])):
-                head_pos = (head_pos[0] - 1, head_pos[1])
-                if not check_distance(head_pos, tail_pos):
-                    tail_pos = (tail_pos[0] - 1, tail_pos[1])
-                    if not check_ordinal(head_pos, tail_pos):
-                        tail_pos = (tail_pos[0], head_pos[1])
-                    if tail_pos not in visited:
-                        visited.append(tail_pos)
-                        print_board()
-print_board()
-print(len(visited))  
-
+                curr_tail = rope[tail_key]
+                print(tail_key)
+                while move(head_pos, curr_tail, dir): # TODO: return new tail pos if true to modify
+                    if tail_key == 9 and curr_tail not in visited:
+                        visited.append(tail_key)
+                    tail_key += 1
+                    if tail_key == 10:
+                        break
+                    else:
+                        curr_tail = rope[tail_key]
+                tail_key += 1
+                print(tail_key)
+    break
 
 
